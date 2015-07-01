557e3d7b90385de84f7cca7db733c6227a0c90f7
unknown <WGetz@WGetz-ELT.hunterindustries.com>
2015-07-01 13:13:31 -0700

    Exercise 1 from "Learn Python the Hard Way"

---
+++ Python/Exercises/Source/ex1.py
@@ -0,0 +1,10 @@
+import Exercises
+
+x = Exercises.Exercises()
+print x.GetPrintStatement("Hello World!")
+print x.GetPrintStatement("Hello Again")
+print x.GetPrintStatement("I like typing this.")
+print x.GetPrintStatement("This is fun.")
+print x.GetPrintStatement("Yay! Printing")
+print x.GetPrintStatement("I'd much rather you 'not'.")
+print x.GetPrintStatement('I "said" do not touch this.')
